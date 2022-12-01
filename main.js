a33cd11eb0fd22ba9e3c862962170eaa183877a9
Victor Andeloci <victor.andeloci@gmail.com>
2022-11-30 21:15:22 -0300

    abrir iframe com jogo

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+// buscar iframe e colocar numa variável
+var iframe = document.querySelector('#play');
+
 // document (página inteira)
 document
   // busca no documento por qualquer TAG A dentro de uma CLASSE "jogo"
@@ -11,6 +14,23 @@ document
 
         // variável LINK recebe o atributo "href" do botao (tag A)
         var link = botao.getAttribute('href');
-        alert(link);
+
+        // puxa a lista de classes do IFRAME e adiciona a class "exibir"
+        iframe.classList.add('exibir');
+        // define um atributo "SRC" no IFRAME com valor de LINK (variável)
+        iframe.setAttribute('src', link);
       });
+    });
+
+// busca em todo document
+document
+  // observando o evento "keypress" (tecla do teclado) e executa uma função
+  .addEventListener('keypress', function(evento) {
+    // SE (condicional) a TECLA (key) do evento for IGUAL (==) a "Escape", executa o código {}
+    if (evento.key == '0') {
+      // puxa a lista de classes do IFRAME e remove a class "exibir"
+      iframe.classList.remove('exibir');
+      // definindo o atributo "SRC" com valor VAZIO ('') no IFRAME
+      iframe.setAttribute('src', '');
+    }
   });
