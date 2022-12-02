098734588b7347b200fc624827922e25370ff28f
Victor Andeloci <victor.andeloci@gmail.com>
2022-12-02 01:32:43 -0300

    load from server

--- main.js
+++ main.js
@@ -1,11 +1,67 @@
+var urlSite = 'https://flixsenac.000webhostapp.com/';
+
+// buscando na URL contida na variável urlSite
+fetch(urlSite, {
+  // método GET pega um valor
+  method: 'GET'
+})
+// "então"... ou seja, assim que a resposta vier
+.then(function(resposta) {
+  // retorna a resposta em texto
+  return resposta.text();
+})
+.then(function(texto) {
+  // retornando o texto convertido em JSON
+  return JSON.parse(texto)
+})
+.then(function(json) {
+
+  // buscar listaJogos e colocar numa variável
+  var listaJogos = document.querySelector('#listaJogos');
+
+  // LOOP (estrutura de repetição) executa um trecho de código pra cada jogo
+  json.forEach((jogo, j) => {
+    // DIV com classe "jogo"
+    var div = document.createElement('div');
+    div.classList.add('jogo');
+
+    // variável IMG com elemento <img>
+    var img = document.createElement('img');
+    // definindo um atributo SRC no elemento IMG
+    img.setAttribute('src', 'https://flixsenac.000webhostapp.com' + jogo.cover);
+    // coloca o elemento IMG na DIV
+    div.appendChild(img);
+
+    // variável H3 com elemento <h3>
+    var h3 = document.createElement('h3');
+    // atribui o título ao h3
+    h3.innerHTML = jogo.title;
+    // coloca o elemento H3 na DIV
+    div.appendChild(h3);
+
+    // variável a com elemento <a>
+    var a = document.createElement('a');
+    // definindo um atributo HREF no elemento A
+    a.setAttribute('href', jogo.url);
+    // atribui o texto ao elemento A
+    a.innerHTML = 'Jogue aqui';
+    // coloca o elemento A na DIV
+    div.appendChild(a);
+
+    // coloca o elemento DIV na #listaJogos
+    listaJogos.appendChild(div);
+  });
+
+});
+
 // buscar iframe e colocar numa variável
 var iframe = document.querySelector('#play');
 
 // document (página inteira)
 document
-  // busca no documento por qualquer TAG A dentro de uma CLASSE "jogo"
+  // busca no documento por qualquer TAG A dentro de uma CLASSE "jogo" e retorna como uma [LISTA]
   .querySelectorAll('.jogo a')
-    // LOOP (estrutura de repetição) pra cada A (botao)
+    // LOOP (estrutura de repetição) executa um trecho de código pra cada A (botao)
     .forEach((botao, i) => {
       // executa um trecho de código (function) pra cada evento "CLICK" no botao
       botao.addEventListener('click', function(evento) {
